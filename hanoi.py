d092e18c6e0470b928041f4b81f3f8e873c9323a
Erimsah Olut <erimsaholut@icloud.com>
2023-06-29 14:28:39 +0300

    py file added

---
+++ hanoi.py
@@ -0,0 +1,16 @@
+def hanoiKuleleri(kaynak,temp,hedef,n):
+    if (n == 0):
+        return
+    
+    hanoiKuleleri(kaynak, hedef, temp, n - 1)
+    print(f"\n{n} disk ({kaynak} -> {hedef})")
+    hanoiKuleleri(temp, kaynak, hedef, n - 1)
+    
+
+
+n = int(input("Number of Disks \n"))
+hanoiKuleleri('a', 'b', 'c', n);
+print("\n")
+
+
+
